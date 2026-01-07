11b52582e50bbd6e7a623ab4e7ff3ef866e40ce6
ミヤザキ <denixden2818@gmail.com>
2026-01-07 15:14:26 -0500

    Added Mobile version of image onto page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -39,6 +39,13 @@ export default function Page() {
             alt='Screenshots of the dashboard project showing desktop version'
             className='hidden md:block'
           />
+          <Image 
+            src='/hero-mobile.png'
+            width={560}
+            height={620}
+            alt='Screenshots of the dashboard project showing mobile version'
+            className='md:hidden sm:block'
+          />
         </div>
       </div>
     </main>
