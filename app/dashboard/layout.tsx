d101ade21850ccb250b22138bd779b80408af5a5
ミヤザキ <denixden2818@gmail.com>
2026-01-07 18:21:21 -0500

    Added Content div in Dashboard Layout

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -6,6 +6,8 @@ export default function Layout({children} : {children: React.ReactNode}){
       <div className="w-full flex-none md:w-64">
         <SideNav /> 
       </div>
+      <div className="grow p-6 md:overflow-y-auto md:p-12">
+      </div>
     </div>
   )
 }
